4d9a39c2e88b1e31312f357abde8b4d338cd3434
Sylvester Salo <sylttysalo@gmail.com>
2021-10-14 15:41:01 +0300

    Hidastus- ja nopeutusnappulat
    Lisätty toimivat nappulat simulaattoriviiveen nopeuttamiseen ja hidastamiseen

---
+++ JavaSimFX/src/simu/view/ISimulaattorinUI.java
@@ -0,0 +1,27 @@
+package simu.view;
+
+import simu.model.Asiakas;
+
+public interface ISimulaattorinUI {
+	
+	// Kontrolleri tarvitsee syötteitä, jotka se välittää Moottorille
+	public double getAika();
+	public long getViive();
+	
+	//Kontrolleri antaa käyttöliittymälle tuloksia, joita Moottori tuottaa 
+	public void setLoppuaika(double aika);
+	
+	// Kontrolleri tarvitsee  
+	public IVisualisointi getVisualisointi();
+	
+	public void setKahvilaJono(Asiakas[] list);
+	public void setKahvila(Asiakas[] list);
+	public void setHyllyt(Asiakas[] list);
+	public void setLihaTiski(Asiakas[] list);
+	public void setLihaJono(Asiakas[] list);
+	public void setIpKassa(Asiakas[] list);
+	public void setIpKassaJono(Asiakas[] list);
+	public void setKassa(Asiakas[] list);
+	public void setKassaJono(Asiakas[] list);
+
+}
